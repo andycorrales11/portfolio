94e51ef7526b0165159178626a1ba1d748974530
Andres Corrales <119633657+andycorrales11@users.noreply.github.com>
2025-07-18 18:37:36 +0000

    Another fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import { withContentlayer } from 'next-contentlayer'
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
